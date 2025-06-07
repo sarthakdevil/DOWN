7ee34bfbcc9b3824db3ce932648a809e2d527ae6
Sarthakdevil <Sarthakrajesh2005@gmail.com>
2025-06-07 12:33:56 +0530

    changes

--- app/api/cloudinary/cloudinary.ts
+++ app/api/cloudinary/cloudinary.ts
@@ -3,9 +3,9 @@ import cloudinary from "cloudinary";
 
 // Configure Cloudinary
 cloudinary.v2.config({
-  cloud_name: process.env.cloud_name,
-  api_key: process.env.api_key,
-  api_secret: process.env.api_secret,
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 export async function uploadToCloudinary(base64File: string) {
